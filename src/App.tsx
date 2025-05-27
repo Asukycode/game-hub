a473e5ba528f5bca915d6c0642f205281292cf51
Asukycode <asamachi540@gmail.com>
2025-05-27 17:42:45 +0100

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import './App.css'
-import {Grid, GridItem, Show} from '@chakra-ui/react'
-
+import { Grid, GridItem, Show, HStack, Image, Text } from '@chakra-ui/react'
+import logo from './assets/logo.webp'
 function App() {
 
 
   return (
     <Grid templateAreas={{
-      base: `"nav" "main"`, 
+      base: `"nav" "main"`,
       lg: `"nav nav" "main aside"`
     }}>
-      <GridItem area='nav' bg='coral'>nav</GridItem>
+      <GridItem area='nav' bg='coral'>
+        <HStack>
+          <Image src={logo} boxSize={'60px'}></Image>
+          <Text fontSize='2xl' fontWeight='bold'>Chakra UI</Text>
+        </HStack>
+      </GridItem>
+
       <GridItem area='main' bg='lightgreen'>main</GridItem>
+
       <Show above='lg'>
         <GridItem area='aside' bg='lightblue'>aside</GridItem>
       </Show>
-
 
     </Grid>
   )
