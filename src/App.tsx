291bc07c19fd67b0678540685539e6b10e830a67
Asukycode <asamachi540@gmail.com>
2025-05-27 16:35:52 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Grid, GridItem, Show} from '@chakra-ui/react'
 
 function App() {
- 
+
 
   return (
-    <>
-     
-    </>
+    <Grid templateAreas={{
+      base: `"nav" "main"`, 
+      lg: `"nav nav" "main aside"`
+    }}>
+      <GridItem area='nav' bg='coral'>nav</GridItem>
+      <GridItem area='main' bg='lightgreen'>main</GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='lightblue'>aside</GridItem>
+      </Show>
+
+
+    </Grid>
   )
 }
 
